80dcf520e6f82cc875667e9c226f3c1d206b738c
Alexandre Costa <alexandre97costa@gmail.com>
2023-10-12 13:54:28 +0100

    Update flowchart.svelte

--- svelte/src/components/flowchart.svelte
+++ svelte/src/components/flowchart.svelte
@@ -1,6 +1,19 @@
 <script>
     import RecurseNode from "./recurse_node.svelte";
-    export let nodes = []
+    import { grabbed_node } from "../stores";
+
+    export let nodes = [];
+
+    // os nodes são responsáveis por editarem o store "grabbed_node";
+    // o flowchart apanha logo a info atualizada, mas não faz nada com esse update;
+
+    // 1. os nodes e lines têm um on:drop, que manda um dispatch com info de onde aconteceu o drop
+    // 2. esse drop navega até aos recurse_node, e eventualmente até ao flowchart
+    // 3. o flowchart então pega no grabbed node e insere-o no seu nodes
+    // 4. por reactividade, os recurse_nodes são todos atualizados, refrescando o flowchart por completo
+
+    // nota: talvez seja appropriado o objeto json ser guardado num store tb,
+    // para que o JsonPreview tb tenha acesso à nova info.
 </script>
 
 <div
