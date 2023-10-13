ff1f66d9ccab690a279ebcd5b5e78548bacc15ff
Alexandre Costa <alexandre97costa@gmail.com>
2023-10-13 13:01:35 +0100

    publish attempt 2

--- svelte/svelte.config.js
+++ svelte/svelte.config.js
@@ -7,8 +7,9 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-			pages: '../docs',
-			assets: '../docs'
+			pages: 'build',
+			assets: 'build',
+			strict: false
 		}),
 		paths: {
 			base: '/json-flowchart'
