ca3cee85c1db022bd3dfeec8ab34fd3e7b9baf1f
Alexandre Costa <alexandre97costa@gmail.com>
2023-10-13 13:02:16 +0100

    Updates

--- README.md
+++
@@ -1,6 +0,0 @@
-# json-flowchart
-Testing grounds for turning a *valid* json file into a flowchart similar to a commit-graph.
-
-## Using this repo
-
-Go live
--- svelte/src/stores.js
+++
@@ -1,7 +0,0 @@
-import { writable } from "svelte/store";
-
-export const isDark = writable(false);
-
-export const grabbed_node_id = writable("");
-export const drop_location = writable();
-export const flowchart = writable({});
